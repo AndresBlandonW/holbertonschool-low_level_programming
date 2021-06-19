250f487303fce56e001a6e11dcfc0c9be34be629
AndresBlandonW <andblandon@hotmail.com>
2021-06-18 21:33:05 -0700

    script 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - print the lowercase alphabet reverse
+*
+* Return: 0
+*/
+int main(void)
+{
+char ar;
+ar = 122;
+
+while  (ar < 97)
+{
+putchar(ar);
+ar--;
+}
+
+putchar('\n');
+return (0);
+}
