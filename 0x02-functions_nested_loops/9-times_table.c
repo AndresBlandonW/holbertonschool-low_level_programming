3dfba743ecc35192f616c5ed947d6ade78a25a39
AndresBlandonW <andblandon@hotmail.com>
2021-06-22 21:27:07 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,19 +7,26 @@
 
 void times_table(void)
 {
-int x;
-int tabla;
+int x, tabla, res;
 
 for (x = 0; x <= 9; x++)
 {
 
-  _putchar('0');
+_putchar('0');
 
 for (tabla = 1; tabla <= 9; tabla++)
 {
 _putchar(',');
 _putchar(' ');
-_putchar((x * tabla) + '0');
+
+res = (x * tabla);
+
+if (res <= 9)
+_putchar(' ');
+else
+_putchar((res / 10) + '0');
+_putchar((res % 10) + '0');
+
 }
 
 _putchar('\n');
