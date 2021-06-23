99c662a8dabccec6816d1f02e46b703d4bda03f3
AndresBlandonW <andblandon@hotmail.com>
2021-06-22 18:02:08 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - print alphabet x10
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+
+char c;
+int x;
+
+while (x <= 10)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+
+_putchar('\n');
+x++;
+}
+
+_putchar('\n');
+
+}
