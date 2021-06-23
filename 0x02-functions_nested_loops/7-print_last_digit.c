172b0841e97c0e3d585a6cf123b968a1ac63db9a
AndresBlandonW <andblandon@hotmail.com>
2021-06-22 20:17:27 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+* print_last_digit - print the last digital of value
+* @x: calculation
+*
+* Return: last digit
+*/
+
+int print_last_digit(int x)
+{
+
+x = (x % 10);
+return (x);
+
+}
