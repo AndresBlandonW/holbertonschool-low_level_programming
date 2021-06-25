91c0669df5046227f5e8c11c5ea254b64cc660ab
AndresBlandonW <andblandon@hotmail.com>
2021-06-24 18:53:46 -0700

    holberton header

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,12 @@
+#ifndef Holberton_file
+#define Holberton_file
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+
+#endif
