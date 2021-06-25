c8cc5deb46a39ce6d34c3ca72c6654907c224b42
AndresBlandonW <andblandon@hotmail.com>
2021-06-24 19:09:00 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* mul - check for digit 0 to 9
+* @a: value 1
+* @b: value 2
+*
+* Return: multiplies
+*/
+
+int mul(int a, int b)
+{
+
+int x;
+
+x = (a * b);
+return (x);
+
+}
