83c2039ad938fad024c1587322cb1da1c3a19c4e
AndresBlandonW <andblandon@hotmail.com>
2021-06-24 19:15:03 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+* print_numbers - prints the number from 0
+*
+* Return: value
+*/
+
+
+void print_numbers(void)
+{
+
+int x;
+
+for (x = 0; x <= 9; x++)
+{
+_putchar(x);
+}
+
+_putchar('\n');
+
+}
