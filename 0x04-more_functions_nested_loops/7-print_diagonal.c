51a5b8ad371c45039b28fe16918c4f48ddeff435
AndresBlandonW <andblandon@hotmail.com>
2021-06-24 20:53:19 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+* print_diagonal - draws a diagonalline
+* @n: size
+*/
+
+void print_diagonal(int n)
+{
+
+int c;
+int x = 92;
+
+if (n > 0)
+{
+
+for (c = 0; c <= n; c++)
+{
+_putchar(x);
+}
+
+}
+
+_putchar('\n');
+
+}
