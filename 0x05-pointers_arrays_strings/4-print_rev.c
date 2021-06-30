98c242425e38fc3d9e546871e7470f2ecb6a9e01
AndresBlandonW <andblandon@hotmail.com>
2021-06-29 20:46:31 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+* print_rev - in reverse
+* @s: string
+*/
+
+void print_rev(char *s)
+{
+
+int len;
+
+len = _strlen(s);
+
+while (len > 0)
+{
+	_putchar(s[len]);
+	len--;
+}
+_putchar('\n');
+
+}
