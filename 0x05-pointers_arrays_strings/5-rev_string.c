ca5e06280c6b40f67cfddbe54528d8b16f68f6bd
AndresBlandonW <andblandon@hotmail.com>
2021-06-29 21:05:26 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+* rev_string - in reverse
+* @s: string
+*/
+
+void rev_string(char *s)
+{
+
+int i, j;
+int len = 0;
+
+
+while (s[len] != '\0')
+{
+len++;
+}
+j = len - 1;
+
+
+for (i = 0; i < len; i++)
+{
+_putchar(s[j]);
+j--;
+}
+
+}
