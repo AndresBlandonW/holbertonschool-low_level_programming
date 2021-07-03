7e85390dc7a4baeb1fdcfb831593afccd2f7ba53
AndresBlandonW <andblandon@hotmail.com>
2021-07-02 21:20:10 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverses the contect
+ * @a: array
+ * @n: num element
+ */
+
+void reverse_array(int *a, int n)
+{
+int i;
+int start = 0;
+int rev;
+
+
+for (i = n - 1; i < n ; i--, start++)
+{
+rev = a[start];
+a[start] = a[i];
+a[i] = rev;
+}
+
