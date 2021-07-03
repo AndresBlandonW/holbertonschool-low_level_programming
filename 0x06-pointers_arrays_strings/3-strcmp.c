8b1d12229f309fd31b640fc132a8d185f38a0d3b
AndresBlandonW <andblandon@hotmail.com>
2021-07-02 20:31:10 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,30 +1,38 @@
 #include "holberton.h"
 
 /**
+ * _strlen - length of string
+ * @s: string
+ *
+ * Return: length
+ */
+int _strlen(char *s)
+{
+int len = 0;
+
+while (*(s + len) != 0)
+{
+len++;
+}
+return (len);
+}
+
+
+/**
  * _strcmp - compares two strings
- * @s1: string 1
+ * @s1: string 1 
  * @s2: string 2
  * Return: compares
  */
 
 int _strcmp(char *s1, char *s2)
 {
-int len_s1, len_s2;
 int lar;
 int i = 0;
 int result = 0;
 
-len_s1 = 0;
-while (*(s1 + len_s1) != 0)
-{
-len_s1++;
-}
-
-len_s2 = 0;
-while (*(s2 + len_s2) != 0)
-{
-len_s2++;
-}
+int len_s1 = _strlen(s1);
+int len_s2 = _strlen(s2);
 
 if (len_s1 > len_s2)
 {
@@ -35,7 +43,7 @@ else
 lar = len_s2;
 }
 
-while (i < lar)
+while (lar > i)
 {
 if (s1[0] == s2[0])
 {
