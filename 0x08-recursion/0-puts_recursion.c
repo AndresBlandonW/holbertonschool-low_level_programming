9f31376887958cb12aadc0c36cd12aa51333292e
AndresBlandonW <andblandon@hotmail.com>
2021-07-07 18:01:12 -0700

    0x08 project recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - print string
+ * @s: string
+ */
+
+void _puts_recursion(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0' ; i++)
+{
+_putchar(s[i]);
+}
+}
