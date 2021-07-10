4fd582d41e07843a9183f5f345f2f48855cd6e27
AndresBlandonW <andblandon@hotmail.com>
2021-07-09 20:00:54 -0700

    project 0x0A with task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* main - your name
+* @argc: argument can
+* @argv: argument values
+*
+* Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+
+printf("%s\n", *argv++);
+(void) argc;
+return (0);
+}
