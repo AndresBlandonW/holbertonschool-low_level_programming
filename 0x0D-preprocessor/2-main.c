bc1c8afb73aec31c1f149a20f8544a6dadd565c4
AndresBlandonW <andblandon@hotmail.com>
2021-07-19 19:47:12 -0700

    task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,13 @@
-#ifndef __FILE__
-#define __FILE__
+#include <stdio.h>
+#include <stdlib.h>
 
-#endif
+/**
+ * main - file name
+ * Return: name of file
+ */
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
