56220316621dd10bdefa5d40133963d9d7947e61
AndresBlandonW <andblandon@hotmail.com>
2021-07-19 20:53:42 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef dog
+#define dog
+
+/**
+ * struct dog - characteris
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: new struct dog
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+
+};
+
+#endif
