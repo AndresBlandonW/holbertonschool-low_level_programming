6b5bb31d4bc295729d3d4ca75220a0dbd11b3367
AndresBlandonW <andblandon@hotmail.com>
2021-07-20 20:10:22 -0700

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+char *_strdup(char *str);
 
 /**
  * new_dog - initialize
@@ -20,10 +21,51 @@ d = malloc(sizeof(dog_t));
 if (d == NULL)
 return (NULL);
 
-d->name = name;
+d->name = _strdup(name);
 d->age = age;
-d->owner = owner;
+d->owner = _strdup(owner);
+if (!d->owner)
+{
+free(d->name);
+free(d);
+}
 
 return (d);
 
+}
+
+/**
+ * _strdup - pointer to a newly
+ * @str: pointer
+ *
+ * Return: pointer
+ */
+
+char *_strdup(char *str)
+{
+
+char *cpy;
+int i = 0;
+
+if (str == NULL)
+{
+return (NULL);
+}
+
+while (str[i] != 0)
+{
+i++;
+}
+
+i++;
+cpy = malloc(sizeof(char) * i);
+
+for (i = 0; str[i] != 0; i++)
+{
+cpy[i] = str[i];
+}
+
+cpy[i] = 0;
+
+return (cpy);
 }
