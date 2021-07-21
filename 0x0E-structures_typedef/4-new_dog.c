f285b33b87a2bb845c5c3b03f8c94d10f329f2b4
AndresBlandonW <andblandon@hotmail.com>
2021-07-20 19:45:39 -0700

    task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - initialize
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Return: new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+dog_t *dog;
+
+dog = malloc(sizeof(dog_t));
+
+if (dog == NULL)
+return (NULL);
+
+dog->name = name;
+dog->age = age;
+dog->owner = owner;
+
+return (dog);
+
+}
