24fde304630a8d93ea24f23d886405ed4e1b75bb
AndresBlandonW <andblandon@hotmail.com>
2021-07-21 19:36:19 -0700

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - given as a parameter
+ * @array: array
+ * @size: size of array
+ * @action: pointer
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+unsigned int i;
+
+for (i = 0; i <= size; i++)
+{
+action(array[i]);
+}
+
+
+}
