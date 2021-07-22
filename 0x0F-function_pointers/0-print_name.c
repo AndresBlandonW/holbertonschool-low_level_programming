b2e23c249321f2079936acb7e2f7b74358edcaee
AndresBlandonW <andblandon@hotmail.com>
2021-07-21 19:11:42 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name
+ * @f: void f
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+
+if (f == NULL)
+return;
+
+if (name == NULL)
+return;
+	
+f(name);
+
+}
