48cedd8c6bddcbf06e539d008525aa6e6bb7eaa7
AndresBlandonW <andblandon@hotmail.com>
2021-07-30 21:14:30 -0700

    task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -34,21 +34,26 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 
 list_t *newNode = malloc(sizeof(list_t));
+
+if (newNode == NULL)
+return (NULL);
+
 newNode->str = strdup(str);
 newNode->len = _strlen(str);
 newNode->next = NULL;
 
 if (*head == NULL)
-*head = newNode;
-else
 {
+*head = newNode;
+return (*head);
+}
+
 list_t *lastNode = *head;
 while (lastNode->next != NULL)
 {
 lastNode = lastNode->next;
 }
 lastNode->next = newNode;
-}
 
 return (newNode);
 }
