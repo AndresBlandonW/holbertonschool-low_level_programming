722327f396e44a792e9ef3f62f15ba6bbc408ffa
AndresBlandonW <andblandon@hotmail.com>
2021-08-03 19:02:33 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint - print all element list
+ * @h: pointer
+ * Return: num of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+
+size_t num = 0;
+
+while (h != NULL)
+{
+        printf("%u\n",h->n);
+        num++;
+        h = h->next;
+        
+}
+
+return (num);
+
+}
