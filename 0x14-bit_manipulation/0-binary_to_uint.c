4389f1768ece3640e10ca4dd101e55a8c1f61022
AndresBlandonW <andblandon@hotmail.com>
2021-08-05 21:27:09 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,9 +2,9 @@
 #include <stddef.h>
 
 /**
- * binary_to_uint -
- * @b: data
- * Return: data
+ * binary_to_uint - convert binary to int
+ * @b: binary data
+ * Return: unsigned int
  */
 
 unsigned int binary_to_uint(const char *b)
@@ -14,14 +14,15 @@ unsigned int dec = 0;
 int i = 0;
 
 if (b == NULL)
-return 0;
+return (0);
 
 while (b[i] == '0' || b[i] == '1')
-{ 
+{
 dec <<= 1;
-dec += b[i]-'0';
+dec += b[i] - '0';
 i++;
 }
-return dec;
+
+return (dec);
 
 }
