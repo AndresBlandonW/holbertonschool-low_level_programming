392606fe37752219a32cd17372d707a8ebb6b53b
AndresBlandonW <andblandon@hotmail.com>
2021-08-09 21:02:01 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,16 +21,13 @@ fd = open(filename, O_RDWR | O_APPEND, 0600);
 if (fd == -1)
 return (-1);
 
-if (text_content == NULL && fd == 1)
+if (text_content == NULL && fd)
 return (1);
-
 
 can = _strlen(text_content);
 wr = write(fd, text_content, can);
 if (wr == -1)
 return (-1);
-
-close(fd);
 
 return (1);
 }
