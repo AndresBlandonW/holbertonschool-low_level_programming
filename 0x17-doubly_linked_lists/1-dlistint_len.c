cdec9f04aefd1914be0579edf5ca191ed8bd7fc5
Andres Blandon <andblandon@hotmail.com>
2021-09-17 22:25:23 +0000

    task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * dlistint_len - number elem of linked l
+ * dlistint_len - number elem of linked
  * @h: linked list
  * Return: num of elemt
  */
@@ -10,10 +10,10 @@ size_t dlistint_len(const dlistint_t *h)
 {
 	size_t len = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		len++;
 		h = h->next;
 	}
-
+	return (len);
 }
