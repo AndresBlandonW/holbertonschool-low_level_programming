33a99dfcf2c3f0ea79d48a878931837fbddd59e6
Andres Blandon <andblandon@hotmail.com>
2021-10-11 04:26:58 +0000

    tk5 print hs

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index;
+	hash_node_t *item;
+	int flag = 0;
+
+	printf("{");
+	for (index = 0; index < ht->size; index++)
+	{
+		if (ht->array[index] != NULL)
+		{
+			for (item = ht->array[index]; item; item = item->next)
+			{
+				if (flag == 1)
+					printf(", ");
+				printf("'%s': '%s'", item->key, item->value);
+				flag = 1;
+			}
+		}
+	}
+	printf("}\n");
+}
